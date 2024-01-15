80f7a08ba5d4d2453857af2527de2a88d0040cc7
Rayan Hassan <98430041+Rayan-H7@users.noreply.github.com>
2024-01-15 21:39:31 +0000

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int eulertotal = 0;
+  int total = 0;
+  for (int number = 0; number < 1000; number = number + 1){
+    if (number % 5 == 0){
+      eulertotal = eulertotal + number;
+      total = eulertotal;
+    } else if (number % 3 ==0){
+      eulertotal = eulertotal + number;
+      total = eulertotal;
+    }else {
+      total = eulertotal;
+    }
+  }
+  cout << "The total is " << eulertotal << ".\n";
+  return 0;
+}
+//End of Program
